2a05f7ad23f75125f12cf1bdead6735f2a7f6734
Utkarsh singh <utkarshsingh.1306@gmail.com>
2020-09-09 19:28:15 +0530

    Update client.c

--- client.c
+++ client.c
@@ -1,79 +1,5 @@
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<signal.h>
-#include<pthread.h>
-#include<sys/types.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<errno.h>
-#include<string.h>
+#include "client.h"
 
-#define LENGTH 2048
-
-// Global variables
-volatile sig_atomic_t flag = 0;
-int sockfd = 0;
-char name[32];
-
-void str_overwrite_stdout() {
-  printf("%s", ">>");
-  fflush(stdout);
-}
-
-void str_trim_lf (char* arr, int length) {
-  int i;
-  for (i = 0; i < length; i++) { 
-    if (arr[i] == '\n') {
-      arr[i] = '\0';
-      break;
-    }
-  }
-}
-
-void catch_ctrl_c_and_exit(int sig) {
-    flag = 1;
-}
-
-void send_msg_handler() {
-	char message[LENGTH] = {};
-	char buffer[LENGTH + 32] = {};
-
-	while(1) {
-		str_overwrite_stdout();
-		fgets(message, LENGTH, stdin);
-		str_trim_lf(message, LENGTH);
-
-		if (strcmp(message, "exit") == 0) {
-				break;
-		} 
-		else {
-			sprintf(buffer, "%s: %s\n", name, message);
-			send(sockfd, buffer, strlen(buffer), 0);
-		}
-
-		bzero(message, LENGTH);
-		bzero(buffer, LENGTH + 32);
-	}
-	catch_ctrl_c_and_exit(2);
-}
-
-void recv_msg_handler() {
-	char message[LENGTH] = {};
-  while (1) {
-		int receive = recv(sockfd, message, LENGTH, 0);
-		if (receive > 0) {
-			printf("%s", message);
-			str_overwrite_stdout();
-		} 
-		else if (receive == 0) {
-				break;
-		} 
-		
-		memset(message, 0, sizeof(message));
-  	}
-}
 
 int main(int argc, char **argv){
 	if(argc != 2){
@@ -92,7 +18,7 @@ int main(int argc, char **argv){
 
 
 	if (strlen(name) > 31 || strlen(name) < 2){
-		printf("Name must be less than 32 and more than 2 characters.\n");
+		printf("Username must be less than 32 and more than 2 characters.\n");
 		return EXIT_FAILURE;
 	}
 
@@ -112,7 +38,7 @@ int main(int argc, char **argv){
 			return EXIT_FAILURE;
 	}
 
-	// Send name
+	// Send name to the server
 	send(sockfd, name, 32, 0);
 
 	printf("--------------- WELCOME : %s ---------------\n", name);
