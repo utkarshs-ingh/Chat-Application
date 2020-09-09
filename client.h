17aa153d7196c7fc8b7161550d9cf92051be122a
Utkarsh singh <utkarshsingh.1306@gmail.com>
2020-09-09 19:29:12 +0530

    Add files via upload

---
+++ client.h
@@ -0,0 +1,77 @@
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<signal.h>
+#include<pthread.h>
+#include<sys/types.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<errno.h>
+#include<string.h>
+
+#define LENGTH 2048
+
+volatile sig_atomic_t flag = 0;
+int sockfd = 0;
+char name[32];
+
+void str_overwrite_stdout() {
+  printf("%s", ">>");
+  fflush(stdout);
+}
+
+void str_trim_lf (char* arr, int length) {
+  int i;
+  for (i = 0; i < length; i++) { 
+    if (arr[i] == '\n') {
+      arr[i] = '\0';
+      break;
+    }
+  }
+}
+
+
+
+void catch_ctrl_c_and_exit(int sig) {
+    flag = 1;
+}
+
+void send_msg_handler() {
+	char message[LENGTH] = {};
+	char buffer[LENGTH + 32] = {};
+
+	while(1) {
+		str_overwrite_stdout();
+		fgets(message, LENGTH, stdin);
+		str_trim_lf(message, LENGTH);
+
+		if (strcmp(message, "exit") == 0) {
+				break;
+		} 
+		else {
+			sprintf(buffer, "%s: %s\n", name, message);
+			send(sockfd, buffer, strlen(buffer), 0);
+		}
+
+		bzero(message, LENGTH);
+		bzero(buffer, LENGTH + 32);
+	}
+	catch_ctrl_c_and_exit(2);
+}
+
+void recv_msg_handler() {
+	char message[LENGTH] = {};
+  while (1) {
+		int receive = recv(sockfd, message, LENGTH, 0);
+		if (receive > 0) {
+			printf("%s", message);
+			str_overwrite_stdout();
+		} 
+		else if (receive == 0) {
+				break;
+		} 
+		
+		memset(message, 0, sizeof(message));
+  	}
+}
